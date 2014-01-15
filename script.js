b7c0e9d3d05dd4e0c691c934a37ab3074d49a176
Kendrick Parks <bordersmbk02@aol.com>
2014-01-15 15:28:12 -0800

    make pets variable into an object and add image to view

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function petsCtrl($scope) {
-	$scope.pets = ["Ben", "Pickle", "Ethel"];
+	$scope.pets = [
+		{name:"Ben", image:"http://www.placekitten.com/150/150",adopted:false},
+		{name:"Pickle", image:"http://www.placekitten.com/151/150",adopted:false},
+		{name:"Ethel", image:"http://www.placekitten.com/152/150",adopted:false}
+	];
 }
