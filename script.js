dc368cd821a5126ab1f8bc9e1f6ca77b825cf4e9
Laurie415 <Laurie@awesome>
2014-09-04 01:27:04 -0700

    initial commit

--- script.js
+++ script.js
@@ -1,20 +1,70 @@
-function petsCtrl($scope) {
-	$scope.newPetName = "";
-	$scope.newPetImage = "";
-	$scope.pets = [
-		{name:"Ben", image:"http://www.placekitten.com/150/150",adopted:false},
-		{name:"Pickle", image:"http://www.placekitten.com/151/150",adopted:false},
-		{name:"Ethel", image:"http://www.placekitten.com/152/150",adopted:false}
-	];
 
-	$scope.addNewPet = function() {
-		if ($scope.newPetName != "" && $scope.newPetImage != "") {
-			$scope.pets.push({name:$scope.newPetName,image:$scope.newPetImage,adopted:false});
-			$scope.newPetName = "";
-			$scope.newPetImage = "";
-		}
+var app = angular.module("shoppingListApp", []);
+
+app.controller('itemsCtrl', function($scope) {
+	//create blank pet variables
+	$scope.newItemName = "";
+	$scope.newItemType = "";
+	$scope.newItemQuantity = "";
+
+	// These are the types available.
+	$scope.itemTypes = ["Food", "Beauty", "Home", "Other"];
+
+	// This acts as our model.
+	$scope.items = [];
+
+	// Function that adds a new item when the form is submitted.
+	$scope.addNewItem = function() {
+		//check to see if any of the form is blank
+		if($scope.newItemName != "" &&
+		   $scope.newItemQuantity != "" &&
+		   $scope.newItemType != "") {
+		   	if(!isNaN($scope.newItemQuantity)) {
+				//add the new item to the items array in our model
+				$scope.items.push({ name: $scope.newItemName,
+								   quantity: $scope.newItemQuantity,
+								   type: $scope.newItemType,
+								   bought: false});
+				//reset the new pet variables
+				$scope.newItemName = "";
+				$scope.newItemQuantity = "";
+				$scope.newItemType = "";
+			}
+			else {
+				alert("Please enter a number in the quantity field.");
+			}
+		}	
+		//else, the form is blank alert the user
 		else {
-			alert('please fill out form');
+			alert("Please fill in both fields.");
 		}
 	};
-}
+
+	// Run a function that removes items.
+	$scope.buyItem = function() {
+		//create a temp variable that we can put all the pets in
+		var tempItems = $scope.items;
+		//remove all the pets from the original variable
+		$scope.items = [];
+
+		//loop through the temp variable with the forEach angular function
+		angular.forEach(tempItems, function(item) {
+			//if the pet has not been adopted put it back into the original variable
+			if(!item.bought) {
+				$scope.items.push(item);
+			}
+		});	
+	};
+	
+	// Create a function that returns the number of remaining items 
+	// that has to be given back.
+	$scope.remainingItems = function () {
+		var remItems = $scope.items.length;
+
+		angular.forEach($scope.items, function(item) {
+			if(item.bought)
+				remItems--;
+		});
+		return remItems;
+	}
+});
